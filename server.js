49163a2308a93d1220e0c85f914b3771579c2da4
Nknky09 <ks.shido@gmail.com>
2024-12-20 18:22:38 -0500

    1.9.63

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
+import { nanoid } from "nanoid";
+
+let jobs = [
+  { id: nanoid(), comapny: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -19,6 +25,40 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
+});
+
+// GET ALL JOBS
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
+});
+
+// CREATE JOB
+app.post("api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ jobs });
+});
+
+// GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find(job => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+});
+
+// EDIT JOB
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+  }
 });
 
 const port = process.env.PORT || 5100;
