97e5b2365a2c4c4799b3a75ffcca13517ae2aabe
Nknky09 <ks.shido@gmail.com>
2024-12-22 01:16:21 -0500

    v.1.9.62 fetch api, try catch

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 const app = express();
+import morgan from "morgan";
+
+try {
+  const response = await fetch(
+    "https://course-api.com/react-useReducer-cart-project"
+  );
+  const cartData = await response.json();
+  console.log(cartData);
+} catch (error) {
+  console.log(error);
+}
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
@@ -12,6 +29,7 @@ app.post("/", (req, res) => {
   res.json({ message: "data received", data: req.body });
 });
 
-app.listen(5100, () => {
-  console.log("server running...");
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}...`);
 });
