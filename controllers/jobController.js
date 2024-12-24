3acef66098202b1f61bbad6653ba336c01291c12
Nknky09 <ks.shido@gmail.com>
2024-12-24 17:31:34 -0500

    v19.79 Set up create job in jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,5 @@
+import Job from "../models/JobModel.js";
+
 import { nanoid } from "nanoid";
 
 let jobs = [
@@ -6,26 +8,19 @@ let jobs = [
 ];
 
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    res.status(400).json({ msg: "please provide company and position" });
-    return;
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
+  const job = await Job.create(req.body);
   res.status(201).json({ job });
 };
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find(job => job.id === id);
+  const job = await Job.findById(id);
   if (!job) {
-    throw new Error("no job with that id");
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
   res.status(200).json({ job });
