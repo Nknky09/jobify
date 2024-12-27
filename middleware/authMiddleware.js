52a378b8f190ff5bcc4755b60f13c4e0b9bcbd3e
Nknky09 <ks.shido@gmail.com>
2024-12-27 12:42:07 -0500

    v1.9.103 Authenticate user setup, verify cookie token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,7 @@
-export const authenticateUser = async (requestAnimationFrame, res, next) => {
-  console.log("auth middleware");
+import { UnauthenticatedError } from "../errors/customErrors.js";
+
+export const authenticateUser = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) throw new UnauthenticatedError("authentication invalid");
   next();
 };
