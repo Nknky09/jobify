55656fa474996a1db70f6255e497745def30e09e
Nknky09 <ks.shido@gmail.com>
2024-12-29 13:51:40 -0500

    v1.10.118 Frontend, form action register with formData API, redirect to login after register complete

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../assets/components";
+import customFetch from "../utils/customFetch";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/auth/register", data);
+    return redirect("/login");
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 const Register = () => {
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
         <FormRow type="text" name="name" defaultValue="john" />
@@ -28,7 +42,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
