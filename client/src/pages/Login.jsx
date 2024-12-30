9fc8d665603582113323e48b818d3ed247e77a5f
Nknky09 <ks.shido@gmail.com>
2024-12-29 22:54:54 -0500

    v1.10.122 Login page setup, same as register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { Form, Link } from "react-router-dom";
+import { Form, Link, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../assets/components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("auth/login", data);
+    toast.success("Login successful");
+    return redirect("/dashboard");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Login</h4>
         <FormRow type="email" name="email" defaultValue="john@gmail.com" />
@@ -14,8 +33,8 @@ const Login = () => {
         <button type="submit" className="btn btn-block">
           submit
         </button>
-        <button type="button" className="btn btn-block">
-          explore the app
+        <button type="button" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "submit"}
         </button>
         <p>
           Not a member yet?
@@ -24,7 +43,7 @@ const Login = () => {
           </Link>
           `
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
