295c0b8e8c9a55ff7398e4ee7e6d4eab8dbb108b
Nknky09 <ks.shido@gmail.com>
2025-01-02 18:22:21 -0500

    v1.12.137 Create link for edit job with dynamic id

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { FormRow, FormRowSelect } from "../assets/components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useLoaderData } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
 
 const EditJob = () => {
   return <h1>EditJob</h1>;
