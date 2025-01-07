d0d6a8bc153f4aabb98943e0a3af6c383cb309bf
Nknky09 <ks.shido@gmail.com>
2025-01-07 18:17:41 -0500

    1.15.159 dynamic stats, jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,6 @@
 import Job from "../models/JobModel.js";
 import { StatusCodes } from "http-status-codes";
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 import day from "dayjs";
 
 import { nanoid } from "nanoid";
@@ -40,10 +40,22 @@ export const deleteJob = async (req, res) => {
 };
 
 export const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$jobStatus", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+  console.log(stats);
+
   const defaultStats = {
-    pending: 22,
-    interview: 11,
-    declined: 4,
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
   };
   let monthlyApplications = [
     {
