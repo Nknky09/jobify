296bda47220025f6cb5e0b400b4d6eae4cd62e75
Nknky09 <ks.shido@gmail.com>
2025-01-10 16:59:57 -0500

    v1.17 update to deploy app

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -59,8 +59,8 @@ const Login = () => {
         <h4>Login</h4>
         {errors?.msg && <p style={{ color: "red" }}>{errors.msg}</p>}
         <p></p>
-        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
-        <FormRow type="password" name="password" defaultValue="secret123" />
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
         <SubmitBtn />
         <button type="button" className="btn btn-block" onClick={loginDemoUser}>
           explore the app
