912e813d0ca8d8f0fac3c19793db697c6c5a2e70
Nknky09 <ks.shido@gmail.com>
2025-01-17 18:19:50 -0500

    v1.18.191 validate user request, and corrected cache

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,26 +12,22 @@ import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
 import SubmitBtn from "../assets/components/SubmitBtn";
 
-export const action = async ({ request }) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  const errors = { msg: "" };
+export const action =
+  queryClient =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
 
-  if (data.password.length < 3) {
-    errors.msg = "password too short";
-    return errors;
-  }
-
-  try {
-    await customFetch.post("auth/login", data);
-    toast.success("Login successful");
-    return redirect("/dashboard");
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    //errors.msg = error?.response?.data?.msg;
-    return error;
-  }
-};
+    try {
+      await customFetch.post("auth/login", data);
+      queryClient.invalidateQueries();
+      toast.success("Login successful");
+      return redirect("/dashboard");
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return error;
+    }
+  };
 
 const Login = () => {
   const errors = useActionData();
