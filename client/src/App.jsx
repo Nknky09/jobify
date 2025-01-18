bc811691dbbd6cfac5d74ceb633ce85de040d882
Nknky09 <ks.shido@gmail.com>
2025-01-18 14:20:15 -0500

    v1.18.192 edit job query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -105,7 +105,7 @@ const router = createBrowserRouter([
           {
             path: "edit-job/:id",
             element: <EditJob />,
-            loader: editJobLoader,
+            loader: editJobLoader(queryClient),
             action: editJobAction,
           },
           {
